c50ce43b4271ac00f0bb3c8e9cc07d0757a6b43a
sdp1992 <soumyadipdutta2007@gmail.com>
2019-03-05 00:37:32 +0530

    Docker Deployment

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use an official Python runtime as a parent image
+FROM python:2.7-slim
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY Dockerfile /app
+COPY requirements.txt /app
+COPY TwitterToKafka.py /app
+COPY config.ini /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# Define environment variable
+ENV NAME World
+
+# Run app.py when the container launches
+CMD ["python", "TwitterToKafka.py"]
--- TwitterToKafka.py
+++ TwitterToKafka.py
@@ -1,5 +1,4 @@
 # Importing libraries
-import socket
 import requests
 import requests_oauthlib
 import json
@@ -7,52 +6,61 @@ from kafka import KafkaProducer
 from kafka.errors import *
 import configparser
 from requests.exceptions import *
-
+import time
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
-# Replace the values below with yours auth credentials
+# Replace the values below with yours auth credentials / Change config.ini file
 ACCESS_TOKEN = config['CREDENTIALS']['ACCESS_TOKEN']
 ACCESS_SECRET = config['CREDENTIALS']['ACCESS_SECRET']
 CONSUMER_KEY = config['CREDENTIALS']['CONSUMER_KEY']
 CONSUMER_SECRET = config['CREDENTIALS']['CONSUMER_SECRET']
 my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
-# Kafka cluster details (single node cluster for dev)
-bootstrap_server_list = ['XXX.XXX.XXX.XXX:9092']
+# Kafka cluster details (single node cluster)
+bootstrap_server_list = ['127.0.0.1:9092']
 TOPIC_NAME = config['DEFAULT']['TOPIC_NAME']
 
 
 # This create a new kafka producer instance
 def connect_kafka_producer():
     _producer = None
-    try:
-        _producer = KafkaProducer(bootstrap_servers=bootstrap_server_list, acks=1, api_version=(0, 10))
-    except Exception as ex:
-        print('Exception while connecting Kafka')
-        print(str(ex))
-    finally:
-        return _producer
+    while _producer is None:
+        try:
+            _producer = KafkaProducer(bootstrap_servers=bootstrap_server_list, client_id = "tweet_receiver",
+                                      retries=1, acks=1)
+
+            if _producer is None:
+                raise Exception
+        except Exception as ex:
+            print('Unable to connect to Kafka.')
+            pass
+        else:
+            return _producer
 
 
 # This function iterates all the tweets and push them to kafka topic
 def publish_message(producer_instance, topic_name, key, value):
-    for line in value.iter_lines():
-        if value is not None:
-            try:
-                key_bytes = bytes(key)
-                try:
-                    full_tweet = json.loads(line)
-                except ValueError as e:
-                    pass
-                value_bytes = bytes(full_tweet['text'].encode('utf8', 'replace'))
-                producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
-                producer_instance.flush()
-                print('Message published successfully.')
-            except requests.exceptions.ConnectionError as e:
-                print("Note able to send messages")
+    while True:
+        try:
+            for line in value.iter_lines():
+                if value is not None:
+                    key_bytes = bytes(key)
+                    try:
+                        full_tweet = json.loads(line)
+                        value_bytes = bytes(full_tweet['text'].encode('utf8', 'replace'))
+                        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
+                        producer_instance.flush(timeout=2)
+                    except KafkaTimeoutError as e:
+                        time.sleep(10)
+                        print("Unable to send messages.")
+                    except Exception as e:
+                        time.sleep(10)
+                        print("Unable to process current message batch.")
+        except Exception:
+            time.sleep(10)
+            print("No message received.")
 
 
 def get_tweets():
@@ -64,13 +72,15 @@ def get_tweets():
         response = requests.get(query_url, auth=my_auth, stream=True, timeout=5)
         print(query_url, str(response.status_code))
     except (ConnectTimeout, HTTPError, ReadTimeout, Timeout, ConnectionError):
-        print("Unable to get data from twitter")
+        print("Unable to get data from twitter.")
+    except Exception as e:
+        print("Something wrong happened.")
     finally:
         return response
 
 
 def app():
-    print("Waiting for TCP connection...")
+    print("Starting the app...")
 
     producer = connect_kafka_producer()
 
