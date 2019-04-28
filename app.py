9086cc4d23b2815c3acf3172a4580f9bd0e0a9bc
sdp1992 <soumyadipdutta2007@gmail.com>
2019-04-29 03:07:56 +0530

    Code refactored to run in docker

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Importing libraries
 from dotenv import load_dotenv
 
-load_dotenv(".env", verbose=True)
+# load_dotenv(".env", verbose=True) Uncomment if not using docker
 
 if __name__ == '__main__':
     from src.main.python.main import *
